f58a5894556f5e921c973cc3383e4bcd7a560a0f
abhishek <abhishek.rauthan.10@gmail.com>
2021-02-28 21:47:18 +0530

    added bulma to the project

--- src/app/lib/lib.module.ts
+++ src/app/lib/lib.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 const Components = [CardComponent]
 
 @NgModule({
-  declarations: [Components],
+  declarations: [Components, SidebarComponent],
   imports: [
     CommonModule
   ],
