085316d9f9f496f6fab2a327b20bd7e44a386368
abhishek <abhishek.rauthan.10@gmail.com>
2021-03-01 22:27:01 +0530

    added various components

---
+++ src/app/module-home/module-home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+@NgModule({
+  declarations: [HeroComponent, HomeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ModuleHomeModule { }
