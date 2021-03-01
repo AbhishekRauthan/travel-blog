8a3b29d23b9e0998b19023cc381ce1246e629281
abhishek <abhishek.rauthan.10@gmail.com>
2021-03-01 18:17:01 +0530

    added a nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HeroComponent } from './hero/hero.component';
-import { LibModule } from './lib/lib.module';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HeroComponent
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    LibModule,
     AppRoutingModule,
   ],
   providers: [],
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showNav: boolean = false;
+
+  toggle() {
+    this.showNav = !this.showNav;
+  }
+
+  classes() {
+    return `navbar-menu ${this.showNav ? 'is-active' : ''}`
+  }
+}
